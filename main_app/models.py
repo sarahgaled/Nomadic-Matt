b2d5653913c6e3dd8437179f9a0d63766c53fc7b
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-25 21:22:38 -0400

    fixing up the Blog model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    place = models.CharField(max_length=200)
+    description = models.TextField(max_length=450)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.place
