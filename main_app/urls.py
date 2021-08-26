c5ab55bff64e72eaa096d4706043d67ae65ebfad
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-25 21:22:49 -0400

    cleaning up paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('places/', views.places_index, name="places_index")
+    path('blogs/', views.blogs_index, name="blog_index"),
+    path('blogs/<int:blog_id>/', views.blogs_detail, name="blogs_detail"),
+    path('blogs/create/', views.BlogCreate.as_view(), name='blogs_create'),
 ]
