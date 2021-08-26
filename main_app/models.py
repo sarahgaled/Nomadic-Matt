cdacda38c82fe446612933e343a2ab924323eb7f
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-26 16:56:21 -0400

    adding a  user

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Blog(models.Model):
     place = models.CharField(max_length=200)
+    date = models.DateField("Trip date")
     description = models.TextField(max_length=450)
-    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.place
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('blogs/', views.blogs_index, name="blogs_index"),
     path('blogs/<int:blog_id>/', views.blogs_detail, name="blogs_detail"),
