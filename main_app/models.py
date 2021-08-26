7d8f8210c98a575a1654fe2417cbc70b762c0439
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-26 11:02:27 -0400

    adding reverse

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,3 +10,7 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.place
+
+    def get_absolute_url(self):
+        return reverse("blogs_detail", kwargs={"blog_id": self.id})
+    
