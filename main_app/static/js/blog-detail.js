82ab85b2634ed0e81be91abd1d2252a74325f3e0
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-26 16:57:29 -0400

    creating date picker

---
+++ main_app/static/js/blog-detail.js
@@ -0,0 +1,17 @@
+console.log('this works!')
+
+
+
+const dateInput = document.getElementById('id_date')
+
+const picker = MCDatepicker.create({
+  el: '#id_date',
+  dateFormat: 'yyyy-mm-dd',
+  closeOnBlur: true,
+  selectedDate: new Date(),
+})
+
+dateInput.addEventListener("click", (evt) => {
+  picker.open()
+})
+
