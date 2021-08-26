1ef5df918a68247642ac469a27e2fffe6c3f2fbb
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-26 16:56:37 -0400

    chaning the home view

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from .models import Blog
 
 
 # Define the home view
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
     return render(request, 'about.html')
@@ -16,16 +17,21 @@ def blogs_index(request):
 
 def blogs_detail(request, blog_id):
     blog = Blog.objects.get(id=blog_id)
-    return render (request, 'blogs/detail.html', {'blog': blog })
+    return render (request, 'blogs/detail.html', {'blog': blog})
 
 class BlogCreate(CreateView):
     model = Blog
     fields = '__all__'
     success_url = '/blogs/'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 class BlogUpdate(UpdateView):
     model = Blog
     fields = ['description', 'date']
+    success_url = '/blogs/'
 
 class BlogDelete(DeleteView):
     model = Blog
