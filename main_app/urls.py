63fb6ce3ce39b0e29a4fba8702370a610d9764d1
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-26 11:02:49 -0400

    update and delete path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('blogs/', views.blogs_index, name="blog_index"),
+    path('blogs/', views.blogs_index, name="blogs_index"),
     path('blogs/<int:blog_id>/', views.blogs_detail, name="blogs_detail"),
     path('blogs/create/', views.BlogCreate.as_view(), name='blogs_create'),
+    path('blogs/<int:pk>/update/', views.BlogUpdate.as_view(), name='blogs_update'),
+    path('blogs/<int:pk>/delete/', views.BlogDelete.as_view(), name='blogs_delete'),
 ]
