14635e0fec703eba281783e4b9ec988f51016c7a
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-30 11:02:44 -0400

    deleting an unnecesary

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('blogs/<int:pk>/update/', views.BlogUpdate.as_view(), name='blogs_update'),
     path('blogs/<int:pk>/delete/', views.BlogDelete.as_view(), name='blogs_delete'),
     path('blogs/<int:blog_id>/add_photo/', views.add_photo, name='add_photo'),
+    path('blogs/<int:blog_id>/delete_photo/<int:photo_id>', views.delete_photo, name='delete_photo'),
     path('accounts/signup/', views.signup, name='signup'),
 
 ]
